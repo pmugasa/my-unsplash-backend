36bdeb027c868f48e64a4b9263da1f2dbf8625a3
Pete Laptop <pmugasa78@gmail.com>
2023-05-21 04:07:11 +0200

    added the dist file

---
+++ index.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Image = require("./models/image");
+const cors = require("cors");
+//mongoDB config
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((error) => {
+    console.error("Couldn't connect to mongoDB:", error.message);
+  });
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
+
+//ROUTES
+app.get("/images", (req, res) => {
+  Image.find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+});
+
+//post image
+app.post("/images", async (req, res) => {
+  const body = req.body;
+  const image = new Image({
+    label: body.label,
+    photoUrl: body.photoUrl,
+  });
+  try {
+    const savedimage = await image.save();
+    res.status(201).json(savedimage);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//delete image
+app.delete("/images/:id", async (req, res) => {
+  const id = req.params.id;
+  await Image.findByIdAndDelete(id);
+  res.status(200).json({ message: "Successfully deleted" });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ models/image.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const imageSchema = new mongoose.Schema({
+  label: {
+    type: String,
+    required: true,
+  },
+  photoUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+imageSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Image = mongoose.model("Image", imageSchema);
+
+module.exports = Image;
